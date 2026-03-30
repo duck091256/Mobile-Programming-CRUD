6ea89243f21aacbcd086beb2a9da6dbdcb09eb21
duc091256 <ductran091256@gmail.com>
2026-03-31 03:47:22 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/voicemind/data/local/VocabDatabase.kt
@@ -0,0 +1,18 @@
+package com.example.voicemind.data.local
+
+import androidx.room3.Database
+import androidx.room3.RoomDatabase
+import androidx.room3.TypeConverters
+import com.example.voicemind.data.local.entity.UserProfileEntity
+import com.example.voicemind.data.local.dao.UserProfileDao
+
+
+@Database(
+    entities = [UserProfileEntity::class],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class VocabDatabase : RoomDatabase() {
+    abstract fun userProfileDao(): UserProfileDao
+}
